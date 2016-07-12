76fdd62c57c42afacd1b932aedff0626c4b02908
kerrot <kerrot.gs@gmail.com>
2016-07-12 12:32:14 +0900

    test shadow

--- Program/Assets/Script/Effect/ShadowEffect.cs
+++ Program/Assets/Script/Effect/ShadowEffect.cs
@@ -2,12 +2,23 @@
 using UniRx.Triggers;
 using System.Linq;
 using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ShadowEffect : MonoBehaviour {
 
     [SerializeField]
     private float time;
+    [SerializeField]
+    private List<MaterialMapping> mapping = new List<MaterialMapping>();
+
+    [Serializable]
+    public struct MaterialMapping
+    {
+        public Material from;
+        public Material to;
+    }
 
     private float start;
 
@@ -15,6 +26,19 @@ public class ShadowEffect : MonoBehaviour {
     {
         this.UpdateAsObservable().Subscribe(_ => UniRxUpdate());
         start = Time.realtimeSinceStartup;
+
+        MeshRenderer[] renders = GetComponentsInChildren<MeshRenderer>();
+        renders.ToList().ForEach(r =>
+        {
+            for (int i = 0; i < r.materials.Length; ++i)
+            {
+                MaterialMapping map = mapping.Find(p => r.materials[i].name.Contains(p.from.name));
+                if (map.to != null)
+                {
+                    r.materials[i] = map.to;
+                }
+            }
+        });
     }
 
     void UniRxUpdate()
