da5e1ffe0d528b81c0748ee3e030fb5b160a38d6
kerrot <kerrot.gs@gmail.com>
2017-01-30 15:46:53 +0900

    Title with OP

---
+++ Program/Assets/Script/Stage/TitleStage.cs
@@ -0,0 +1,75 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TitleStage : MonoBehaviour
+{
+    [SerializeField]
+    private MovieTexture mv;
+    [SerializeField]
+    private GameObject PV;
+    [SerializeField]
+    private Image cover;
+    [SerializeField]
+    private float period;
+    [SerializeField]
+    private Button btn;
+
+    private AudioSource audioSource;
+    private Animator anim;
+
+    System.IDisposable subject;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = mv.audioClip;
+
+        OPPlay();
+    }
+
+    void OPStart()
+    {
+        audioSource.Play();
+        mv.Play();
+
+        if (subject != null)
+        {
+            subject.Dispose();
+        }
+        subject = this.UpdateAsObservable().Where(_ => Input.anyKey).Subscribe(_ => OPEnd());
+    }
+
+    void OPEnd()
+    {
+        anim.enabled = false;
+        if (subject != null)
+        {
+            subject.Dispose();
+        }
+
+        PV.SetActive(false);
+        cover.color = Color.clear;
+        cover.raycastTarget = false;
+        audioSource.Stop();
+        mv.Stop();
+
+        OPPlay();
+    }
+
+    void OPPlay()
+    {
+        Observable.Timer(System.TimeSpan.FromSeconds(period)).Subscribe(_ =>
+        {
+            anim.enabled = true;
+            anim.Play("Base Layer.OP", 0, 0);
+        });
+
+        subject = this.UpdateAsObservable().Where(_ => Input.GetButtonDown("Attack")).Subscribe(_ => btn.onClick.Invoke());
+    }
+}
