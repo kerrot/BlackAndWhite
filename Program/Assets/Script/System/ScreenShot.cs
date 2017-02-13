681902a36794229c01c6bddadcc78cbd974bffde
kerrot <kerrot.gs@gmail.com>
2017-02-14 00:17:41 +0900

    famituu page

---
+++ Program/Assets/Script/System/ScreenShot.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour {
+
+    [SerializeField]
+    private string filename;
+
+    private void Start()
+    {
+        this.UpdateAsObservable().Where(_ => Input.GetMouseButtonDown(0))
+                                 .Subscribe(_ => Application.CaptureScreenshot(filename));
+    }
+}
