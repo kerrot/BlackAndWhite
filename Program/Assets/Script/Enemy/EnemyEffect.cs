65bdc212aca390f9c847c6e609088919c88da379
kerrot <kerrot.gs@gmail.com>
2017-02-21 18:23:32 +0900

    enemy shakeeffect

---
+++ Program/Assets/Script/Enemy/EnemyEffect.cs
@@ -0,0 +1,20 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyEffect : MonoBehaviour
+{
+    [SerializeField]
+    private EnemyBattle battle;
+    [SerializeField]
+    private ShakeEffect shake;
+
+    // Use this for initialization
+    void Start ()
+    {
+        battle.OnAttacked.Subscribe(_ => shake.enabled = true).AddTo(this);	
+	}
+	
+}
